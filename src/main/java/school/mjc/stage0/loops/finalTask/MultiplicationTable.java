0ea71bd536196a51eed3d29d43fede3bfbf830b5
OlzhasAlseitov <olzhas010111@gmail.com>
2023-11-21 21:06:09 +0600

    Stage 0 - Module 4.2 - final task - 7

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.finalTask;
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
         if (numberTableToPrint <= 0) {
-            return numberTableToPrint;
+            throw new IllegalArgumentException("Number must be a positive integer.");
         }
 
         for (int i = 1; i <= 10; i++) {
