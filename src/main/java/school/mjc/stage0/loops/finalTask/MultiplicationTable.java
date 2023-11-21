acb9a47c9bf7cd9a766624f3db55c2da034f99fc
OlzhasAlseitov <olzhas010111@gmail.com>
2023-11-21 20:49:33 +0600

    Stage 0 - Module 4.2 - final task - 1

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+        if (numberTableToPrint <= 0) {
+            throw new IllegalArgumentException("Number must be a positive integer.");
+        }
+
+        for (int i = 1; i <= 10; i++) {
+            int result = numberTableToPrint * i;
+            System.out.println(numberTableToPrint + " * " + i + " = " + result);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
+        if (power < 0) {
+            System.out.println("Power should be a non-negative integer.");
+            return;
+        }
 
+        int result = 1;
+
+        // Calculate power using a for loop
+        for (int i = 1; i <= power; i++) {
+            result *= numberToPrint;
+        }
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
